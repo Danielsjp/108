528f8d8aa98b022fe71e47f8a89ecf05e07baa35
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-04-26 12:16:47 +0200

    .
    .

--- src/n1exercici1/Principal.java
+++ src/n1exercici1/Principal.java
@@ -1,17 +1,29 @@
 package n1exercici1;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(checkIfStringsHaveSubStrings("abcxde", "xyz"));
-		System.out.println(checkIfStringsHaveSubStrings("abcde", "xyz"));
-
-	}
-
-	public static String checkIfStringsHaveSubStrings(String s1, String s2) {
-		return s1.chars().filter(c -> s2.contains((char) c + "")) .findFirst().isEmpty() ? "La cadena: "+s1+" no contiene la letra "+s2 : s1;
+	
+		/*
+		 * ArrayList<String> lista1 = new ArrayList<>(); ArrayList<String> lista2 = new
+		 * ArrayList<>(); lista1.add("manzana"); lista1.add("platano");
+		 * lista1.add("tomate");
+		 */
 		
-	}
+		List<String> lines = Arrays.asList("spring", "node", "mkyong");
 
+        List<String> result = lines.stream()                // convert list to stream
+                .filter(line -> line.contains("o") && line.length()>4)     // we dont like mkyong
+                .collect(Collectors.toList());              // collect the output and convert streams to a List
+        		result.forEach(System.out::println); 
+  	
+    }
 }
